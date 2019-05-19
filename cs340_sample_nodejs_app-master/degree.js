e7eb87127a2b53a2625d2cf9f43957fd4830e6a7
malingyan2017 <maling@oregonstate.edu>
2019-05-19 12:19:35 -0700

    add degree

--- cs340_sample_nodejs_app-master/degree.js
+++ cs340_sample_nodejs_app-master/degree.js
@@ -16,29 +16,19 @@ module.exports = function(){
     /*Display all degrees. Requires web based javascript to delete users with AJAX*/
 
     router.get('/', function(req, res){
-	//console.log('start get /');
-        //var callbackCount = 0;
         var context = {};
         //context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         var mysql = req.app.get('mysql');
         getDegree(res, mysql, context, complete);
         function complete(){
-            //callbackCount++;
-            //if(callbackCount >= 2){
-               // console.log("check point1")
-                res.render('degree', context);
-            //}
-
+            res.render('degree', context);
         }
     });
       /* Adds a degree, redirects to the degree page after adding */
-
       router.post('/', function(req, res){
-        console.log(req.body.homeworld)
-        console.log(req.body)
         var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO degree (degree_type, field) VALUES (?, ?)";
-        var inserts = [req.body.type];
+        var sql = "INSERT INTO degree (degree_type, field) VALUES (?,?)";
+        var inserts = [req.body.degree_type, req.body.field];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 console.log(JSON.stringify(error))
